2727d2d71a7395c11e75f7ebc5dfe64aadbbf9e0
= <zaroslikob@mail.ru>
2022-09-16 18:15:45 +0300

    Задание 2, выполненно

---
+++ src/WordsChecker.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordsChecker {
+    private String text;
+    public WordsChecker (String text){
+        this.text = text;
+    }
+
+    boolean hasWord(String word){
+        Set<String> set = new HashSet<>();
+        String [] infoText = text.split("\\P{IsAlphabetic}+");
+        set.addAll(List.of(infoText));
+        if (set.contains(word)){
+            return true;
+        }
+        return false;
+    }
+}
