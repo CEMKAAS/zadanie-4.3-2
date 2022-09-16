3e9081d2a0e3ac694895ee6a0ad822f2f9c5d418
= <zaroslikob@mail.ru>
2022-09-16 18:17:54 +0300

    Реформат кода

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -4,15 +4,16 @@ import java.util.Set;
 
 public class WordsChecker {
     private String text;
-    public WordsChecker (String text){
+
+    public WordsChecker(String text) {
         this.text = text;
     }
 
-    boolean hasWord(String word){
+    boolean hasWord(String word) {
         Set<String> set = new HashSet<>();
-        String [] infoText = text.split("\\P{IsAlphabetic}+");
+        String[] infoText = text.split("\\P{IsAlphabetic}+");
         set.addAll(List.of(infoText));
-        if (set.contains(word)){
+        if (set.contains(word)) {
             return true;
         }
         return false;
